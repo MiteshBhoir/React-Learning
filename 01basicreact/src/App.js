9ac18ca91a22e490987d5bfcc1325aaa6490df83
Mitesh Bhoir <miteshbhoirofficial@gmail.com>
2025-06-13 23:44:12 +0530

    Created react using two methods vite@latest and basic npx create-react-app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+ 
+function App() {
+  return (
+   <>
+   <h1>Hello Myself mitesh Bhoir| i am a software engineer</h1>
+   </>
+  );
+}
+
+export default App;
---
+++ 01vite/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <><h1>Mitesh bhoir | Full stack developer</h1>
+    <p>This is me learning react from beginning</p>
+    </>
+  )
+}
+
+export default App
